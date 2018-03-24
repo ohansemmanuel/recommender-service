31967c8d1daa51f2fe433841ecd47deec6d0e617
ohansemmanuel <ohansemmanuel@gmail.com>
2018-03-24 21:05:43 +0100

    initiate resource  authentication

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 import os
 from flask import Flask
 from flask_restful import Api
+from flask_jwt import JWT
 import pandas as pd
 import numpy as np
-#from utils.matrix_factorization_utilities import low_rank_matrix_factorization
 
+from security import authenticate, identity
 from resources.recommend import RecommendedMovies
 from resources.movies import Movies
 from resources.movie import Movie
+from dotenv import load_dotenv
 
+load_dotenv(verbose=True)
 app = Flask(__name__)
-
 # load configs
 app.config.update(
-    DEBUG=True
+    DEBUG=True,
+    SECRET_KEY=os.getenv('SECRET_KEY')
 )
 
 api = Api(app)
+
+#auth
+jwt = JWT(app, authenticate, identity)
 
 api.add_resource(RecommendedMovies, '/recommend/movies/<int:user_id>')
 api.add_resource(Movies, '/movies')
